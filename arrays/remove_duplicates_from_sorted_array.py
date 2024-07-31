66e1220d62bc92e37b000fcaf92ba585a98d6678
Илья Ковыршин <gravity4849@gmail.com>
2024-07-31 09:51:39 +0300

    remove duplicates update

--- arrays/remove_duplicates_from_sorted_array.py
+++ arrays/remove_duplicates_from_sorted_array.py
@@ -22,9 +22,37 @@ class Solution:
         return replace_index
 
 
+class NewSolution:
+
+    def removeDublicates(self, nums: list[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+        index0 = 0
+        index1 = 1
+        k = 0
+        while index1 < len(nums):
+            if nums[index0] == nums[index1] and k > 0:
+                ...
+            elif nums[index0] == nums[index1]:
+                k += 1
+                index0 += 1
+                nums[index0] = nums[index1]
+            elif nums[index0] != nums[index1]:
+                k = 0
+                index0 += 1
+                nums[index0] = nums[index1]
+            index1 += 1
+        return index0 + 1
+
+
 class TestRemoveDuplicatesFromArray:
 
     def test_remove_duplicates_from_array_01(self):
         nums = [1, 1, 1, 2, 2, 3]
         Solution().removeDuplicates(nums)
+        assert nums == [1, 1, 2, 2, 3, 3]
+
+    def test_new_remove_duplicates_from_array_02(self):
+        nums = [1, 1, 1, 2, 2, 3]
+        NewSolution().removeDublicates(nums)
         assert nums == [1, 1, 2, 2, 3, 3]
