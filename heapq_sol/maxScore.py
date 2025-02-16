295ee193f6ac1fb71ba285398980d108eab6f556
Илья Ковыршин <gravity4849@gmail.com>
2025-02-16 08:32:11 +0300

    maxScore heap alg

---
+++ heapq_sol/maxScore.py
@@ -0,0 +1,27 @@
+import heapq
+
+
+class Solution:
+    def maxScore(self, nums1, nums2, k):
+        result = 0
+        totalSum = 0
+        heap = []
+
+        merged = [(nums2[i], nums1[i]) for i in range(len(nums1))]
+        merged.sort(reverse=True)
+
+        for maxOf2, num1Val in merged:
+            if len(heap) == k:
+                totalSum -= heapq.heappop(heap)
+
+            totalSum += num1Val
+            heapq.heappush(heap, num1Val)
+
+            if len(heap) == k:
+                result = max(result, totalSum * maxOf2)
+
+        return result
+
+
+if __name__ == '__main__':
+    Solution().maxScore([1, 3, 3, 2], [2, 1, 3, 4], 3)
